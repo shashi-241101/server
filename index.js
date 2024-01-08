1c4137263680e401124e956fd73b9bc7c69545a8
Shashi Pandey <kr.shashi123pandey@gmail.com>
2024-01-08 19:04:19 +0530

    Social_server

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
-
+app.get("/", (req, res) => {
+ res.send("Server is running");
+ });
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 
